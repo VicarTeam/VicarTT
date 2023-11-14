24ebb96f1b12bef79abdf8532632e037cf4f45f6
DasDarki <rageskilln@gmail.com>
2023-11-14 21:59:27 +0100

    finish integration

--- README.md
+++ README.md
@@ -1,22 +1,71 @@
-# FoundryVTT Module Template w/ Typescript
+![](https://img.shields.io/badge/Foundry-v10-informational)
+<!--- Downloads @ Latest Badge -->
+<!--- replace <user>/<repo> with your username/repository -->
+<!--- ![Latest Release Download Count](https://img.shields.io/github/downloads/<user>/<repo>/latest/module.zip) -->
 
-This repo is meant to be used as a starting point for creating your own FoundryVTT module with [Typescript][2]. If you are using Github you can get started by clicking the green `Use this template` button in the upper-right.
-
-Check out [our blog post][4] for a walkthrough of the codebase.
-
-## What's in the box
-
-Out of the box this template adds a button to the top of the Actors directory. Clicking it brings up a modal with a button that will load a picture of a random dog from the [Dog API][3]. This demonstrates how to perform some common tasks such as render templates and call external APIs, and hopefully provides a decent starting point for developing your own module.
-
-## Todo
-
-If you have just created a new project with this template there are a few changes you should make to start:
-
-- [ ] Update the values in `src/module.json`. At minimum you should change `id`, `title`, and `description`. It is also recommended that you add a `contacts` field.
-- [ ] Start using a new module prefix for localizations. The localizations in `src/languages/en.json` are all prefixed with `MYMODULE.` to distinguish them from any other installed translations. You should choose a new prefix for your module and use it for any new translations you add, and remove the existing translation entries as they become unnecessary.
+<!--- Forge Bazaar Install % Badge -->
+<!--- replace <your-module-name> with the `name` in your manifest -->
+<!--- ![Forge Installs](https://img.shields.io/badge/dynamic/json?label=Forge%20Installs&query=package.installs&suffix=%25&url=https%3A%2F%2Fforge-vtt.com%2Fapi%2Fbazaar%2Fpackage%2F<your-module-name>&colorB=4aa94a) -->
 
 
-[1]: https://foundryvtt.com/
-[2]: https://www.typescriptlang.org/
-[3]: https://dog.ceo/dog-api/
-[4]: https://bringingfire.com/blog/intro-to-foundry-module-development
+# How to use this Template to create a versioned Release
+
+1. Open your repository's releases page.
+
+![Where to click to open repository releases.](https://user-images.githubusercontent.com/7644614/93409301-9fd25080-f864-11ea-9e0c-bdd09e4418e4.png)
+
+2. Click "Draft a new release"
+
+![Draft a new release button.](https://user-images.githubusercontent.com/7644614/93409364-c1333c80-f864-11ea-89f1-abfcb18a8d9f.png)
+
+3. Fill out the release version as the tag name.
+
+If you want to add details at this stage you can, or you can always come back later and edit them.
+
+![Release Creation Form](https://user-images.githubusercontent.com/7644614/93409543-225b1000-f865-11ea-9a19-f1906a724421.png)
+
+4. Hit submit.
+
+5. Wait a few minutes.
+
+A Github Action will run to populate the `module.json` and `module.zip` with the correct urls that you can then use to distribute this release. You can check on its status in the "Actions" tab.
+
+![Actions Tab](https://user-images.githubusercontent.com/7644614/93409820-c1800780-f865-11ea-8c6b-c3792e35e0c8.png)
+
+6. Grab the module.json url from the release's details page.
+
+![image](https://user-images.githubusercontent.com/7644614/93409960-10c63800-f866-11ea-83f6-270cc5d10b71.png)
+
+This `module.json` will only ever point at this release's `module.zip`, making it useful for sharing a specific version for compatibility purposes.
+
+7. You can use the url `https://github.com/<user>/<repo>/releases/latest/download/module.json` to refer to the manifest.
+
+This is the url you want to use to install the module typically, as it will get updated automatically.
+
+# How to List Your Releases on Package Admin
+
+To request a package listing for your first release, go to the [Package Submission Form](https://foundryvtt.com/packages/submit) (accessible via a link at the bottom of the "[Systems and Modules](https://foundryvtt.com/packages/)" page on the Foundry website).
+
+Fill in the form. "Package Name" must match the name in the module manifest.  Package Title will be the display name for the package.  Package URL should be your repo URL.
+![image](https://user-images.githubusercontent.com/36359784/120664263-b49e5500-c482-11eb-9126-af7006389903.png)
+
+
+One of the Foundry staff will typically get back to you with an approval or any further questions within a few days, and give you access to the package admin pages.
+
+Once you have access to the [module admin page](https://foundryvtt.com/admin/packages/package/), you can release a new version by going into the page for your module, scrolling to the bottom, and filling in a new Package Version.
+
+When listing a new version, Version should be the version number you set above, and the Manifest URL should be the manifest __for that specific version__ (do not use /latest/ here).
+![image](https://user-images.githubusercontent.com/36359784/120664346-c4b63480-c482-11eb-9d8b-731b50d70939.png)
+
+> ### :warning: Important :warning:
+> 
+> It is very important that you use the specific release manifest url, and not the `/latest` url here. For more details about why this is important and how Foundry Installs/Updates packages, read [this wiki article](https://foundryvtt.wiki/en/development/guides/releases-and-history).
+
+Clicking "Save" in the bottom right will save the new version, which means that anyone installing your module from within Foundry will get that version, and a post will be generated in the #release-announcements channel on the official Foundry VTT Discord.
+
+
+# FoundryVTT Module
+
+Does something, probably
+
+## Changelog
---
+++ scripts/module.js
@@ -0,0 +1,260 @@
+let relayClient = undefined;
+
+const ROLL_OUTCOME = {
+  CRITICAL_SUCCESS: ['Critical Success', '#469B48'],
+  MESSY_CRITICAL: ['Messy Critical', '#2E4B36'],
+  SUCCESS: ['Success', '#026202'],
+  FAILURE: ['Failure', 'red'],
+  BESTIAL_FAILURE: ['Bestial Failure', 'darkred']
+};
+
+function generateRandomHash() {
+  const uuidv4 = () => {
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+      const r = Math.random() * 16 | 0;
+      const v = c === 'x' ? r : (r & 0x3 | 0x8);
+      return v.toString(16);
+    });
+  }
+
+  const id1 = uuidv4().replace(/-/g, '');
+  const id2 = Date.now().toString(16);
+  const id3 = Math.floor(Math.random() * 1000000000).toString(16);
+  const hash = `${id3}${id1}${id2}`;
+  return hash.split('').sort(() => Math.random() - 0.5).join('');
+}
+
+Hooks.once('init', async function() {
+  game.settings.register('vicartt', 'isIntegrationEnabled', {
+    name: 'Enable Integration',
+    hint: 'Enable the VicarTT integration',
+    scope: 'client',
+    config: true,
+    type: Boolean,
+    default: false,
+    onChange: value => {
+      console.log('VicarTT: Integration enabled: ', value);
+      if (value) {
+        console.log('VicarTT: Connecting to VicarTT...');
+        const defaultUrl = localStorage.getItem('vicartt-url') || '';
+        const defaultHash = localStorage.getItem('vicartt-hash') || generateRandomHash();
+
+        const dialog = new Dialog({
+          title: 'VicarTT Integration',
+          content: `<p>Enter the URL for your VicarTT instance and copy&paste the FoundryVTT VicarTT ID into Vicar.</p><p>URL: <input id="vicartt-url" type="text" value="${defaultUrl}"/></p><p>FoundryVTT VicarTT ID: <input id="vicartt-hash" type="text" value="${defaultHash}"/></p>`,
+          buttons: {
+            ok: {
+              icon: '<i class="fas fa-check"></i>',
+              label: 'OK',
+              callback: () => {
+                const url = document.getElementById('vicartt-url').value;
+                const hash = document.getElementById('vicartt-hash').value;
+                localStorage.setItem('vicartt-url', url);
+                localStorage.setItem('vicartt-hash', hash);
+                relayClient = new RelayClient();
+                relayClient.connect();
+              }
+            },
+            cancel: {
+              icon: '<i class="fas fa-times"></i>',
+              label: 'Cancel'
+            }
+          }
+        });
+        dialog.render(true);
+      } else {
+        if (relayClient) {
+          relayClient.stop();
+          relayClient = undefined;
+        }
+      }
+    }
+  });
+
+  const isIntegrationEnabled = game.settings.get('vicartt', 'isIntegrationEnabled');
+  if (isIntegrationEnabled) {
+    console.log('VicarTT: Connecting to VicarTT...');
+    relayClient = new RelayClient();
+    relayClient.connect();
+  }
+});
+
+class RelayClient {
+  constructor() {
+    this._stopped = false;
+    this._url = localStorage.getItem('vicartt-url');
+    this._hash = localStorage.getItem('vicartt-hash');
+    this._source = undefined;
+    this._sourceReconnectController = undefined;
+
+    if (this._url.endsWith('/')) {
+      this._url = this._url.slice(0, -1);
+    }
+  }
+
+  connect() {
+    if (this._sourceReconnectController) {
+      clearInterval(this._sourceReconnectController);
+    }
+
+    if (this._stopped) {
+      return;
+    }
+
+    this._source = new EventSource(`${this._url}/vicartt/stream?accessKey=${this._hash}`);
+    this._source.onmessage = this.#onMessage.bind(this);
+    this._source.onerror = this.#onError.bind(this);
+    this._sourceReconnectController = setInterval(this.#checkConnection.bind(this), 5000);
+  }
+
+  stop() {
+    this._stopped = true;
+    this._source.close();
+    clearInterval(this._sourceReconnectController);
+  }
+
+  #onMessage(event) {
+    const data = JSON.parse(event.data);
+    VampiricDiceRoller.roll(data);
+  }
+
+  #onError(event) {
+    console.warn('VicarTT: Connection error: ', event);
+  }
+
+  #checkConnection() {
+    if (this._source.readyState === 2) {
+      this.connect();
+    }
+  }
+}
+
+class VampiricDiceRoller {
+  static async roll(data) {
+    const roll = new Roll(this.#formatRollText(data.roll));
+    await roll.evaluate({
+      async: true
+    });
+
+    const msg = await roll.toMessage();
+    const html = await msg.getHTML();
+    const messageElement = html.get(0);
+    const messageContent = messageElement.querySelector('div.message-content');
+
+    let usernameElement = ``;
+    if (data.username && data.username !== '') {
+      usernameElement = `<i style="font-size: 0.8rem">${data.username}</i>`;
+    }
+
+    if (data.vampire.avatar && data.vampire.avatar !== '') {
+      messageContent.innerHTML = `<div style="display: flex; align-items: center; gap: 0.5rem; margin-bottom: 0.5rem; margin-top: 0.3rem"><img src="${data.vampire.avatar}" style="width: 3rem; height: 3rem"><div style="display: flex; flex-direction: column; align-items: center; justify-content: flex-start"><span style="font-size: 1.1rem; font-weight: bold; width: 100%; text-align: left">${data.vampire.name}</span>${usernameElement}</div></div>` + messageContent.innerHTML;
+    } else {
+      messageContent.innerHTML = `<div style="display: flex; align-items: center; gap: 0.5rem; margin-bottom: 0.5rem; margin-top: 0.3rem"><div style="display: flex; flex-direction: column; align-items: center; justify-content: flex-start"><span style="font-size: 1.1rem; font-weight: bold; width: 100%; text-align: left">${data.vampire.name}</span>${usernameElement}</div></div>` + messageContent.innerHTML;
+    }
+
+    const formulaElement = messageContent.querySelector('div.dice-formula');
+    const formulaParts = [];
+    if (data.roll.normalDices > 0) {
+      formulaParts.push(`${data.roll.normalDices} normal dice`);
+    }
+    if (data.roll.hungerDices > 0) {
+      formulaParts.push(`${data.roll.hungerDices} hunger dice`);
+    }
+
+    formulaElement.innerHTML = formulaParts.join(' + ');
+
+    let totalSuccesses = 0;
+    let totalCriticals = 0;
+    let totalFailures = 0;
+    let hungerCriticals = 0;
+    let hungerFailures = 0;
+
+    const handleDicePart = (section) => {
+      const formula = section.querySelector('span.part-formula');
+      const isHunger = formula.innerHTML.endsWith('dg');
+
+      const total = section.querySelector('span.part-total');
+      const rolls = section.querySelectorAll('ol.dice-rolls > li.roll');
+      let successes = 0;
+      let criticals = 0;
+
+      rolls.forEach(roll => {
+        const value = parseInt(roll.innerText);
+        if (value >= 6) {
+          successes++;
+        }
+        if (value === 10) {
+          criticals++;
+
+          if (isHunger) {
+            hungerCriticals++;
+          }
+        } else if (value === 1) {
+          totalFailures++;
+
+          if (isHunger) {
+            hungerFailures++;
+          }
+        }
+      });
+
+      totalSuccesses += successes;
+      totalCriticals += criticals;
+
+      formula.innerText = isHunger ? 'Hunger Dice' : 'Normal Dice';
+      total.innerText = successes + ' (' + total.innerText + ')';
+    };
+
+    const diceParts = messageContent.querySelectorAll('section.tooltip-part');
+    diceParts.forEach(handleDicePart);
+
+    if (totalCriticals >= 2) {
+      const pairs = Math.floor(totalCriticals / 2);
+      totalSuccesses += pairs * 2;
+    }
+
+    const diceTotal = messageContent.querySelector('h4.dice-total');
+    diceTotal.innerText = totalSuccesses + ' (' + diceTotal.innerText + ')';
+
+    const outcome = this.#analyzeRollOutcome(totalSuccesses, totalCriticals, totalFailures, hungerCriticals, hungerFailures, data.roll.difficulty || 1);
+    const difficultyText = data.roll.difficulty ? ` (vs. ${data.roll.difficulty})` : '';
+
+    const diceResult = messageContent.querySelector('div.dice-result');
+    diceResult.innerHTML += `<h4 class="dice-total" style="color: ${outcome[1]};">${(outcome[0] + difficultyText)}</h4>`;
+
+    await msg.update({
+      content: messageContent.innerHTML
+    });
+  }
+
+  static #analyzeRollOutcome(successes, criticals, failures, hungerCriticals, hungerFailures, difficulty) {
+    const isSuccess = successes >= difficulty;
+    const isCritical = criticals >= 2;
+    const isMessy = hungerCriticals >= 1;
+    const isBestial = hungerFailures >= 1;
+
+    if (isSuccess && isCritical && isMessy) {
+      return ROLL_OUTCOME.MESSY_CRITICAL;
+    } else if (isSuccess && isCritical) {
+      return ROLL_OUTCOME.CRITICAL_SUCCESS;
+    } else if (isSuccess) {
+      return ROLL_OUTCOME.SUCCESS;
+    } else if (isBestial) {
+      return ROLL_OUTCOME.BESTIAL_FAILURE;
+    } else {
+      return ROLL_OUTCOME.FAILURE;
+    }
+  }
+
+  static #formatRollText(rollData) {
+    const diceTexts = [];
+    const {normalDices, hungerDices} = rollData;
+    if (normalDices > 0) {
+      diceTexts.push(`${normalDices}dv`);
+    }
+    if (hungerDices > 0) {
+      diceTexts.push(`${hungerDices}dg`);
+    }
+    return diceTexts.join(' + ');
+  }
+}
